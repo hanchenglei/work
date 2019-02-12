69b408fee798b99b09fece757e33fc5966a46cd5
linux <xx@xx.com>
2019-02-11 22:45:21 -0800

    socket test

---
+++ server.c
@@ -0,0 +1,76 @@
+/*
+ *personnel_mannagement/server
+ *hcl 2019/2/12
+ *v 0.0
+ */
+
+#include "head.h"
+
+struct sockaddr_in local_addr;
+socklen_t loacl_addrlen = sizeof(local_addr);
+struct sockaddr_in perr_addr;
+socklen_t perr_addrlen  = sizeof(perr_addr); 
+
+void * myfunc (void *arg)
+{
+	char buf[64] = {};
+	while(1)
+	{
+	    memset(buf,0,sizeof(buf));
+		recv((int)arg,buf,sizeof(buf),0);
+		printf("buf = %s\n",buf);
+	}
+}
+
+int main(void)
+{
+	int acceptfd;
+	int socketfd;
+	int ret = 0;
+	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(-1 == socketfd)
+	{
+		perror("socket");
+		return -1;
+	}
+    local_addr.sin_family = AF_INET;
+	local_addr.sin_port     = htons(8888);
+	local_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
+
+    ret = bind(socketfd,(const struct sockaddr *)&local_addr,loacl_addrlen);
+	if(-1 == ret)
+	{
+		perror("bind");
+		return -1;
+	}
+    ret = listen(socketfd,10);
+	if(-1 == ret)
+	{
+		perror("listen");
+		return -1;
+	}
+    
+	printf("************************\n");
+	printf("*listen.......\n");
+	printf("************************\n");
+	while(1)
+	{
+		acceptfd = accept(socketfd,(struct sockaddr *)&perr_addr,&perr_addrlen);
+		if(-1 == acceptfd)
+		{
+			perror("accept");
+			return -1;
+		}
+		char *perr_ip = inet_ntoa(perr_addr.sin_addr);
+    	printf("**********************************\n");
+    	printf("*%s accept....\n",perr_ip);
+    	printf("**********************************\n");
+        pthread_t thread;
+		if(0 != pthread_create(&thread,NULL,myfunc,(void*)acceptfd))
+		{
+			perror("pthread_create");
+			return -1;
+		}
+	}
+	return 0;
+}
